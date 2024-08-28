bdd03b4ea864e6f52a74afe2010c79c8ab7b4a11
Burt <154017141+Burt8@users.noreply.github.com>
2024-08-27 23:34:19 -0400

    change return value position, swap names, new func
    changed the position of where the final result would be returned
    created a new function that loops the length of the array which always sorts
    swapped names of the methods to be more fitting

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,8 +1,7 @@
 example_array = [3,4,72,23,43,5,82,4,6,71,4,3,1]
 
-def bubble_sort(array)
+def sorter(array)
   unsorted_array = array
-  until previous_sortment == unsorted_array do
     unsorted_array.each_with_index do |number, position|
       unless array[-1] == number
         if unsorted_array[position] > unsorted_array[(position + 1)]
@@ -11,10 +10,15 @@ def bubble_sort(array)
           unsorted_array[position] = less
           unsorted_array[(position + 1)] = greater
         end
+      end
     end
-  end
 end
-  p unsorted_array
+
+def bubble_sort(array)
+  array.map{
+    sorter(array)
+  }
+  p array
 end
 
 bubble_sort(example_array)
